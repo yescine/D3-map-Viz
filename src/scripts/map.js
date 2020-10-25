67af5f725c6ec830dd74c3b6b8c018da55306d24
yescine <bouchoucha.yassine@gmail.com>
2020-10-25 18:06:06 +0100

    add interaction to map

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,21 +1,42 @@
 import * as d3 from 'd3';
 import {feature} from 'topojson'
+
 const svg = d3.select('.face')
 const heigth = +svg.attr('height')
 const width = +svg.attr('width')
+let countryName=null;
 
-const projection = d3.geoMercator();
-// const projection = d3.geoNaturalEarth1();
-
+// const projection = d3.geoMercator();
+const projection = d3.geoNaturalEarth1();
 const pathGenerator = d3.geoPath().projection(projection)
+
+const g = svg.append('g');
+g.append('path')
+    .attr('class', 'sphere')
+    .attr('d', pathGenerator({type: 'Sphere'}));
+
+svg.call(d3.zoom().on('zoom', () => {
+  g.attr('transform', d3.event.transform);
+}));
+
+  d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv')
+    .then(tsvData => {
+      countryName = tsvData.reduce((accumulator, d) => {
+      accumulator[d.iso_n3] = d.name;
+      return accumulator;
+      }, {})
+});
+
 d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
   .then(data=>{
     const countries = feature(data,data.objects.countries)
 
-    const paths = svg.selectAll('path')
+    const paths = g.selectAll('path')
       .data(countries.features)
-
-    paths.enter().append('path').attr('d',d=>pathGenerator(d))
-      .style('fill','green')
-      .style('stroke','black')
+      .enter().append('path')
+        .attr('class', 'country')
+        .attr('title','hello')
+        .attr('d',d=>pathGenerator(d))
+      .append('title')
+        .text(d=>countryName[d.id])
   })
