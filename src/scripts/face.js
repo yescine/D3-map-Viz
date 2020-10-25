843c3a140b4ac4a5307b2f2e9684c1cc11cb76cd
yescine <bouchoucha.yassine@gmail.com>
2020-10-25 11:34:02 +0100

    finish face

--- src/scripts/face.js
+++ src/scripts/face.js
@@ -1,34 +1,44 @@
 import * as d3 from 'd3';
 
-const svg = d3.select('svg')
+const svg = d3.select('.face')
 const heigth = +svg.attr('height')
 const width = +svg.attr('width')
+const g = svg.append('g')
+  .attr('transform',`translate(${width/2},${heigth/2})`)
 
-const circle = svg
+const circle = g
   .append('circle')
   .attr('r',heigth/3)
-  .attr('cx',width/2)
-  .attr('cy',heigth/2)
   .attr('fill','yellow')
   .attr('stroke','black')
 
 const eyeSpacing = 60
-const eyeLeft = svg
-  .append('circle')
-  .attr('r',heigth/20)
-  .attr('cx',width/2 - eyeSpacing)
-  .attr('cy',heigth/2 - eyeSpacing + 10)
-  .attr('fill','black')
+const eyeRadius = heigth/20
+const eyeG = g.append('g')
+  .attr('transform',`translate(0,${eyeSpacing -120})`)
 
-const eyeRight = svg
+const eyeLeft = eyeG
   .append('circle')
-  .attr('r',heigth/20)
-  .attr('cx',width/2 + eyeSpacing)
-  .attr('cy',heigth/2 - eyeSpacing + 10)
-  .attr('fill','black')
+  .attr('r',eyeRadius)
+  .attr('cx',- eyeSpacing)
 
-const g = svg.append('g')
-  .attr('transform',`translate(${width/2},${heigth/2})`)
+const eyeRight = eyeG
+  .append('circle')
+  .attr('r',eyeRadius)
+  .attr('cx',+ eyeSpacing)
+const eyeBrowWidth = 60
+const leftEyeBrow = eyeG.append('rect')
+  .attr('x',-eyeSpacing - eyeBrowWidth/2)
+  .attr('y', -50)
+  .attr('width',eyeBrowWidth)
+  .attr('height',eyeBrowWidth/4)
+
+const rightEyeBrow = eyeG.append('rect')
+  .attr('x',eyeSpacing - eyeBrowWidth/2)
+  .attr('y', -50)
+  .attr('width',eyeBrowWidth)
+  .attr('height',eyeBrowWidth/4)
+
 const mouth = g.append('path')
   .attr('d',d3.arc()({
     innerRadius:80,
